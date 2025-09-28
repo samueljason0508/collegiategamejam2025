5615a85b451e959a07583a3495e784afb798c269
Badmuffinjr <145946676+Badmuffinjr@users.noreply.github.com>
2025-09-27 21:29:39 -0400

    Adjusted PlayButtonScript
    Added MusicObject and adjusted PlayButtonScript

--- Assets/Scripts/PlayButtonScript.cs
+++ Assets/Scripts/PlayButtonScript.cs
@@ -1,15 +1,70 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayButtonScript : MonoBehaviour
 {
-    // This function will be called when the PlayButton is clicked
+    [Header("Music Settings")]
+    [SerializeField] private string resourcesPath = "Audio/Menu-1"; // Assets/Resources/Audio/Menu-1.wav
+    [SerializeField, Range(0f,1f)] private float volume = 0.7f;
+    [SerializeField] private bool playOnStart = true;
+    [SerializeField] private bool fadeOutOnPlay = false;
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    [Header("Scene Settings")]
+    [SerializeField] private string sceneName = "MainMenuPlay";
+
+    private AudioSource src;
+
+    void Awake()
+    {
+        src = GetComponent<AudioSource>();
+        if (!src) src = gameObject.AddComponent<AudioSource>();
+
+        src.loop = true;
+        src.playOnAwake = false;
+        src.spatialBlend = 0f;  // 2D sound
+        src.volume = volume;
+
+        var clip = Resources.Load<AudioClip>(resourcesPath);
+        if (!clip)
+        {
+            Debug.LogError($"PlayButtonScript: Could not find AudioClip at Resources/{resourcesPath}");
+            return;
+        }
+
+        src.clip = clip;
+        if (playOnStart) src.Play();
+    }
+
+    // Called when the Play button is clicked
     public void LoadGameplayScene()
     {
-        // Debug to make sure the button click works
         Debug.Log("Play button clicked!");
 
-        // Load the gameplay scene by name
-        SceneManager.LoadScene("MainMenuPlay"); // <-- EXACT name of your target scene
+        if (fadeOutOnPlay && src && src.isPlaying)
+        {
+            StartCoroutine(FadeOutThenLoad(sceneName, fadeDuration));
+        }
+        else
+        {
+            if (src && src.isPlaying) src.Stop();
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    private IEnumerator FadeOutThenLoad(string targetScene, float duration)
+    {
+        float startVol = src.volume;
+        float t = 0f;
+        while (t < duration)
+        {
+            t += Time.unscaledDeltaTime;
+            src.volume = Mathf.Lerp(startVol, 0f, t / duration);
+            yield return null;
+        }
+        src.Stop();
+        src.volume = startVol; // reset volume for next time
+        SceneManager.LoadScene(targetScene);
     }
 }
